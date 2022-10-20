3c338e554fb230b1ccf4dd5f17a2155e5128a78e
MariusB5 <marius.briscut5@gmail.com>
2022-10-20 19:48:48 -0400

    scraper finished

--- online_website.py
+++ online_website.py
@@ -1,29 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
+from time import sleep
 
 
-response = requests.get('https://fr.jooble.org/SearchResult?loc=2&rgns=Ile-de-France&ukw=qa+engineer').text
-soup = BeautifulSoup(response, 'lxml')
+def find_jobs():
+    response = requests.get('https://fr.jooble.org/SearchResult?loc=2&ukw=qa+engineer').text
+    soup = BeautifulSoup(response, 'lxml')
 
-articles = soup.find_all('article', class_='FxQpvm yKsady')
+    articles = soup.find_all('article', class_='FxQpvm yKsady')
+    for index, article in enumerate(articles):
 
-for article in articles:
-    matches = ['jour', 'jours', 'heure', 'heures']
-    date_published = article.find('div', class_='caption e0VAhp').text
+        matches = ['jour', 'jours', 'heure', 'heures']
+        date_published = article.find('div', class_='caption e0VAhp').text
+        if any(x in date_published for x in matches):
 
-    if any(x in date_published for x in matches):
-        job = article.find('a', class_='jkit__4ouL4 jkit__300pp hyperlink_appearance_undefined jkit__3uS8R _2dWEc6').text
-        company = article.find('p', class_='Ya0gV9').text
-        location = article.find('div', class_='caption _2_Ab4T').text
-        remote = article.find('div', class_='_2Hplhb').text
+            job = article.find('a', class_='jkit__4ouL4')
+            company = article.find('p', class_='Ya0gV9').text
+            location = article.find('div', class_='caption _2_Ab4T').text
 
-        if remote in ['Travail à distance', 'Télétravail']:
-            remote_status = 'Yes'
-        else:
-            remote_status = 'No'
+            remote = article.find('div', class_='_2Hplhb').text
+            if remote in ['Travail à distance', 'Télétravail']:
+                remote_status = 'Yes'
+            else:
+                remote_status = 'No'
 
-        print(f'Company name: {company}')
-        print(f'Role: {job}')
-        print(f'Location: {location}')
-        print(f'Remote: {remote_status}')
-        print(f'Date published: {date_published[1:]}\n')
+            with open(f'files/{index}.txt', 'w') as f:
+                f.write(f'Company name: {company}\n')
+                f.write(f'Role: {job.text}\n')
+                f.write(f'Location: {location}\n')
+                f.write(f'Remote: {remote_status}\n')
+                f.write(f'Date published: {date_published[1:]}\n')
+                f.write(f"More info: {job['href']}")
+
+            print(f'File saved as {index}.txt')
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        minutes = 1440
+        print(f'Waiting {minutes} minutes...')
+        sleep(minutes * 60)
