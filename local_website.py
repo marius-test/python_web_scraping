29415698d82c3fdbf8ad2bbd05fd81531bbcc686
MariusB5 <marius.briscut5@gmail.com>
2022-10-25 15:18:48 -0400

    python 3.11

--- local_website.py
+++ local_website.py
@@ -11,4 +11,4 @@ with open('home.html', 'r') as html_file:
         course_name = course.h5.text
         course_price = course.a.text.split()[-1]
 
-        print(f'{course_name} + costs {course_price}.')
+        print(f'{course_name} costs {course_price}.')
